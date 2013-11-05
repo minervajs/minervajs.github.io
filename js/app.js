bf55100a50c8a46972c776c2b71aef76e932a25e
David Robbins <robbins.david@gmail.com>
2013-11-05 09:24:06 -0600

    Checkpointing before trying again..

--- js/app.js
+++ js/app.js
@@ -10,15 +10,57 @@
     });
 
     app.factory('libraries', function (angularFireCollection, fbRoot) {
-        return angularFireCollection(fbRoot);// + 'libraries');
+        var libraryRef, libraryCollection;
+        libraryRef = fbRoot.child('libraries');
+        libraryCollection = angularFireCollection(libraryRef);
+        libraryCollection.addLibrary = function (library, cb) {
+            var ref = libraryRef;
+            if (!cb) {
+                ref.child(library.name).child('latest').set(library);
+            } else {
+                ref.child(library.name).child('latest').set(library, cb);
+            }
+        };
+        return libraryCollection;
     });
 
     app.controller('libraryList', function (libraries, $scope) {
+        $scope.user = {};
+        $scope.$on("angularFireAuth:login", function(evt, user) {
+            $scope.user = user;
+        });
+        $scope.$on("angularFireAuth:logout", function(evt, user) {
+            $scope.user = null;
+        });
         $scope.libraries = libraries;
     });
 
-    app.controller('account', function ($scope, fbRoot, angularFireAuth) {
-        angularFireAuth.initialize(fbRoot, {scope: $scope, name: "user"});
+    app.controller('libraryNew', function (libraries, $scope, $location) {
+        $scope.user = {};
+        $scope.$on("angularFireAuth:login", function(evt, user) {
+            $scope.user = user;
+        });
+        $scope.$on("angularFireAuth:logout", function(evt, user) {
+            $scope.user = null;
+        });
+        $scope.saveLibrary = function () {
+            $scope.library.maintainer = {
+                "name": $scope.user.username,
+                "provider": $scope.user.provider,
+                "email": $scope.user.email
+            }
+            libraries.addLibrary($scope.library);
+            $location.path('/libraries');
+        };
+    });
+
+    app.controller('libraryEdit', function (angularFire, fbRoot, $scope, $routeParams) {
+        var ref = fbRoot.child('libraries').child($routeParams.name).child('latest');
+        angularFire(ref, $scope, 'library');
+    });
+
+    app.controller('account', function ($scope, fbRoot, angularFireAuth, $templateCache) {
+        angularFireAuth.initialize(fbRoot, {scope: $scope, name: "user", path: "/login"});
         $scope.loginGithub = function () {
             angularFireAuth.login('github');
         };
@@ -29,7 +71,10 @@
 
     app.config( function ($routeProvider) {
         $routeProvider
+            .when("/login", { controller: 'account', templateUrl: 'login.html' })
             .when("/libraries", { controller: 'libraryList', templateUrl: 'libraryList.html'})
+            .when("/libraries/new", { controller: 'libraryNew', templateUrl: 'libraryEdit.html'})
+            .when("/libraries/:name", { controller: 'libraryEdit', templateUrl: 'libraryEdit.html'})
             .otherwise({ redirectTo: "/libraries"});
     });
 })();
